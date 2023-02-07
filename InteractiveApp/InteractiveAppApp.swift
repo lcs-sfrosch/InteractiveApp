d9739911e666f75a33d2758f694a243ba19b4019
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-02-07 08:51:28 -0500

    Initial Commit

---
+++ InteractiveApp/InteractiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAppApp.swift
+//  InteractiveApp
+//
+//  Created by Sebastian Frosch on 07.02.23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
